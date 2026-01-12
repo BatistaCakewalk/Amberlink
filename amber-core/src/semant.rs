972b6f3c23dc8f7f3a349ea76d1b6746916064d5
Batista <118333712+BatistaCakewalk@users.noreply.github.com>
2026-01-12 01:45:38 -0500

    Update semant.rs

--- amber-core/src/semant.rs
+++ amber-core/src/semant.rs
@@ -1,22 +1,17 @@
 // amber-core/src/semant.rs
 use std::collections::HashMap;
 
-pub struct SymbolTable {
-    // Maps function names to their return types and parameters
-    functions: HashMap<String, FunctionInfo>,
+pub struct FunctionInfo {
+    pub name: String,
+    pub address: u32, // Where it exists in the bytecode
 }
 
-pub struct FunctionInfo {
-    pub params: Vec<String>, // e.g., ["Int", "Int"]
-    pub return_type: String, // e.g., "Int"
+pub struct SymbolTable {
+    pub functions: HashMap<String, FunctionInfo>,
 }
 
 impl SymbolTable {
     pub fn new() -> Self {
         Self { functions: HashMap::new() }
-    }
-
-    pub fn define_func(&mut self, name: String, info: FunctionInfo) {
-        self.functions.insert(name, info);
     }
 }
